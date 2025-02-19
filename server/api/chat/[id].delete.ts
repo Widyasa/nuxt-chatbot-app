d82506f3207e5766dee0dd32a9b78a2110c03dd6
Widya Yasa <108384245+Widyasa@users.noreply.github.com>
2025-02-20 01:28:50 +0800

    feat:add delete message while chat is deleted

--- server/api/chat/[id].delete.ts
+++ server/api/chat/[id].delete.ts
@@ -2,6 +2,11 @@ import prisma from "~/lib/prisma";
 
 export default defineEventHandler(async (event) => {
     try {
+        await prisma.messages.deleteMany({
+            where: {
+                chat_id: event.context.params.id
+            }
+        })
         return await prisma.chats.delete({
             where: {
                 id: event.context.params.id
